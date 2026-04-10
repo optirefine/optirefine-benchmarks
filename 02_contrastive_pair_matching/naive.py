cbc92dd3bd43fb35760b36167d6efaa164976682
optirefine <optirefine1@gmail.com>
2026-04-10 14:27:46 -0400

    Update naive.py

--- 02_contrastive_pair_matching/naive.py
+++ 02_contrastive_pair_matching/naive.py
@@ -1,1 +1,11 @@
-
+def find_positive_pairs(batch_labels):
+    pairs = []
+    batch_size = len(batch_labels)
+    
+    # Comparing every label against every other label
+    for i in range(batch_size):
+        for j in range(i + 1, batch_size):
+            if batch_labels[i] == batch_labels[j]:
+                pairs.append((i, j))
+                
+    return pairs
