ba9ac22aff355022cca93fdd26e34479fb206df5
optirefine <optirefine1@gmail.com>
2026-04-10 14:28:28 -0400

    Update naive.py

--- 03_sequence_tokenization/naive.py
+++ 03_sequence_tokenization/naive.py
@@ -1,1 +1,10 @@
-
+def tokenize_sequences(dataset, delimiter="|"):
+    processed_data = []
+    for sequence in dataset:
+        token_string = ""
+        # String immutability causes memory reallocation on every +=
+        for char in sequence:
+            token_string += char + delimiter
+        processed_data.append(token_string[:-1])
+        
+    return processed_data
