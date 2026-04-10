66fd8990d955786c6f2f94a41c5d789ff81ac3b6
optirefine <optirefine1@gmail.com>
2026-04-10 14:27:14 -0400

    Update optimized.py

--- 01_state_dict_filtering/optimized.py
+++ 01_state_dict_filtering/optimized.py
@@ -1,1 +1,8 @@
-
+def clean_state_dict(loaded_dict, expected_keys):
+    # O(n) Dictionary comprehension
+    new_dict = {k.replace("module.", ""): v for k, v in loaded_dict.items()}
+    
+    # O(1) lookups using Set difference
+    missing_keys = list(set(expected_keys) - set(new_dict.keys()))
+            
+    return new_dict, missing_keys
