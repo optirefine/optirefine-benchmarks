2e1184754e0e25f6626ba75e1281c2355ae9a03f
optirefine <optirefine1@gmail.com>
2026-04-10 14:28:43 -0400

    Update optimized.py

--- 03_sequence_tokenization/optimized.py
+++ 03_sequence_tokenization/optimized.py
@@ -1,1 +1,8 @@
-
+def tokenize_sequences(dataset, delimiter="|"):
+    processed_data = []
+    for sequence in dataset:
+        # Pre-allocates memory and joins in a single C-level operation
+        token_string = delimiter.join(sequence)
+        processed_data.append(token_string)
+        
+    return processed_data
